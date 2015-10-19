4c7ca198e978661d4c0060230c7ec8b9e65ca99d
birgimad <birgitte92madsen@gmail.com>
2015-10-19 13:07:31 +0200

    Code for (b)

--- cpp/Project3/main.cpp
+++ cpp/Project3/main.cpp
@@ -3,29 +3,10 @@
 using namespace std;
 using namespace arma;
 
-/*
-double legendre(int n, double x){
-
-        // This is eq 5.11 from chapter 5.3.1 recursive relation
-    double L_plus = 1 , L = 0, L_minus;
-
-
-    for (int i = 0; i< n; i++)
-    {
-        L_minus = L;
-        L = L_plus;
-        L_plus = (2*i +1)*x*L - i*L_minus ;
-        L_plus /= i+1;
-
-    }
-
-
-    return L_plus;
-}
-*/
-
+//   The following function computes
+//   the weights and function values for the use of the gauss-legendre method
 double GaussianLegendre(double upperlimit, double lowerlimit, double x[], double w[], int n){
-// using info from 5.3.5
+// using info from 5.3.5 to change limits to go from -1 to 1
     double a = ((lowerlimit-upperlimit)/2) ;
     double b = (lowerlimit + upperlimit)/2;
     //double t = a*x + b;
@@ -45,14 +26,11 @@ double GaussianLegendre(double upperlimit, double lowerlimit, double x[], double
     x_temp2 = x + n - 1;
     w_temp2 = w + n - 1;
 
-
     for (int i = 1; i<m;i++){
-        root = cos(pi * (4*i-1) / (4*n + 2 ));
-
-
+        root = cos(pi * (4*i-1) / (4*n + 2 ));  //approximation of the root of the n'th polynomial
     do{
-            // This is eq 5.11 from chapter 5.3.1 recursive relation
-            double L_plus = 1.0 , L = 0.0, L_minus;
+        // This is eq 5.11 from chapter 5.3.1 recursive relation to compute the Legendre polynomials
+        double L_plus = 1.0 , L = 0.0, L_minus;
         for (int j = 0; j < n; j++)
         {
         L_minus = L;
@@ -61,20 +39,18 @@ double GaussianLegendre(double upperlimit, double lowerlimit, double x[], double
         L_plus /= j+1;
         }
 
-        dev_L = (-n*root*L_plus + n*L)/(1-root*root);
+        dev_L = (-n*root*L_plus + n*L)/(1-root*root);   //derivative of the Legendre polynomial (L_plus)
 
         z = root;
         root  = z - L_plus/dev_L;                   // Newton's method
        } while(fabs(root - z) > pow(10,-6));
 
-
+        //compute values of x and w
         * w_temp1 = a*2/((1-root*root)*(dev_L*dev_L));
         *(x_temp1++) = a*root+b;
         *(x_temp2--) = -a*root+b;
         *(w_temp2--) = *(w_temp1++);
-
     }
-
 }
 
 double int_function(double x1, double x2, double x3, double y1, double y2, double y3)
@@ -86,45 +62,28 @@ double int_function(double x1, double x2, double x3, double y1, double y2, doubl
   else return exponent/denominator;
 }
 
-double arybla(double x[])
-{
-    double *x_2;
-    x_2 = x;
-    for (int i = 0; i<10; i++)
-    {
-        *(x_2++) = x[i]+3;
-    }
-}
-
 int main()
 {
+     int n;
+     double upper_limit;
+     double lower_limit;
+     cout << "Please enter value of n:\n>";
+     cin >> n;
+     cout << "Please enter value of lower integral limit:\n>";
+     cin >> lower_limit;
+     cout << "Please enter value of upper integral limit:\n>";
+     cin >> upper_limit;
+     cout << "n = " << n << endl;
+     cout << "lower limit = " << lower_limit << endl;
+     cout << "upper limit = " << upper_limit << endl;
 
-//double limit = 5;
-//double alpha = 2;
-//   weights and function values for the use of the gauss-legendre
-//   method
-     int n = 30;
      double x[n] = {};
      double w[n]={};
-     double upper_limit = 5;
-     double lower_limit = -5;
 
-
-//   set up the mesh points and weights
+//   Computing the mesh points and weights
 GaussianLegendre(lower_limit,upper_limit,x,w,n);
-/*
-cout << "x" << endl;
-for (int j = 0; j<n; j++)
-{
-    cout << x[j] << endl;
-}
-cout << "w=" << endl;
-for (int j = 0; j<n; j++)
-{
-    cout << w[j] << endl;
-}
-*/
 
+//  Computing the result from the computed mesh points and weights
 double int_GaussianLegendre = 0;
 
 for (int f = 0; f<n; f++){
@@ -136,25 +95,7 @@ for (int l = 0; l<n; l++){
     int_GaussianLegendre += w[f]*w[g]*w[i]*w[j]*w[k]*w[l]*int_function(x[f],x[g],x[i],x[j],x[k],x[l]);
 }}}}}}
 
-cout << "result=" << int_GaussianLegendre << endl;
+cout << "Integral value = " << int_GaussianLegendre << endl;
 
-/*
-double int_gauss = 0.;
-for ( int i = 0;  i < n; i++){
-   int_gauss+=w[i]*int_function(x[i]);
-}
-
-    for (int j = 0; j<10; j++)
-    {
-        cout << x[j] << endl;
-    }
-
-    arybla(x);
-
-    for (int j = 0; j<10; j++)
-    {
-        cout << x[j] << endl;
-    }
-*/
     return 0;
 }
