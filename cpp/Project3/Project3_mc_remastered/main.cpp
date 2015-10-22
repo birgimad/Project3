19001248500de858bf72992c5f7af217b314c8ba
birgimad <birgitte92madsen@gmail.com>
2015-10-22 14:29:37 +0200

    blah

--- cpp/Project3/Project3_mc_remastered/main.cpp
+++ cpp/Project3/Project3_mc_remastered/main.cpp
@@ -32,10 +32,7 @@ int main()
 
         //double x[N]={};
 
-
-
         srand(time(NULL));
-
 
         for (int i=0;i<N;i++){
             for (int j=0;j<6;j++){
@@ -47,18 +44,17 @@ int main()
 
             fx = func(x[0],x[1],x[2],x[3],x[4],x[5]);
             px = probvalu(x[0],x[1],x[2],x[3],x[4],x[5], ((double) N));
-            Montec += fx/px;
+            Montec += fx/px/((double) N);
             Montesqr += (fx/px)*(fx/px);
 
        }
 
-       double MC = Montec/((double) N );
+       double MC = Montec;  // /((double) N )
        double Montesqr1= Montesqr/((double) N);
        double variance= Montesqr1-MC*MC;
        double volume = pow(length,6);
        cout << " Integral = " << jacobidet*MC << endl;
        cout << "sigma = " << volume*sqrt(variance/ ((double) N)) << endl;
-
 
 
       return 0;
@@ -68,10 +64,10 @@ int main()
            double alpha = 2.;
 
            double drdr = r1*r1*r2*r2*(-sin(theta1))*(-sin(theta2));
-           double denominator = sqrt(r1*r1+r2*r2-2*r1*r2*(cos(theta1)*cos(theta2)+sin(theta1)*sin(theta2)*cos(phi1-phi2)));
+           double denominator = r1*r1+r2*r2-2*r1*r2*(cos(theta1)*cos(theta2)+sin(theta1)*sin(theta2)*cos(phi1-phi2));
            double exponential = exp(-2.0*alpha*(r1+r2));
            if (fabs(denominator) < pow(10,-6)){return 0;}
-           else return drdr*exponential/denominator;
+           else return drdr*exponential/sqrt(denominator);
         }
 
 double probvalu(double x1,double x2, double x3, double x4, double x5, double x6, double N){
@@ -85,3 +81,5 @@ double probvalu(double x1,double x2, double x3, double x4, double x5, double x6,
     //cout << x1 << setw(20) <<exp(-(y1))<<endl;
     return exp(-(y1+y2+y3+y4+y5+y6));
 }
+
+
